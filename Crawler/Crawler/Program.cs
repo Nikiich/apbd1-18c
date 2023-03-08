0942ab9f4429d067b978c4eb1adc4818f5272276
s22283 <s22283@pjwstk.edu.pl>
2023-03-08 09:37:15 +0100

    init

---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Crawler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 2;
+            Console.WriteLine(a + 5);
+        }
+    }
+}
